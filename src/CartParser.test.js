a2a5d7c1e28e5b589eea0862e746ef7d2e801bee
Dariya Baletskaya <baletskaya.dariya@gmail.com>
2019-07-19 22:21:12 +0300

    add unit testd for CartParser class

---
+++ src/CartParser.test.js
@@ -0,0 +1,105 @@
+import CartParser from "./CartParser";
+//import { isMainThread } from "worker_threads";
+
+//replace with your own absolute path to file cart-errors.csv
+const ABSOLUTE_PATH = "C:/Users/Dariya/Desktop/BSA8";
+let parser, validate, parseLine, parse, readFile;
+let errorHeaderSample,
+  errorRowCellsSample,
+  errorCellStringSample,
+  errorCellNumberSample;
+let contentHeaderError,
+  contentRowCellsError,
+  contentCellStringError,
+  contentCellNumberError,
+  csvLine;
+
+beforeAll(() => {
+  errorHeaderSample = `${ABSOLUTE_PATH}/BSA2019-Testing/samples/cart-header-error.csv`;
+  errorRowCellsSample = `${ABSOLUTE_PATH}/BSA2019-Testing/samples/cart-rowcells-error.csv`;
+  errorCellStringSample = `${ABSOLUTE_PATH}/BSA2019-Testing/samples/cart-cell-string-error.csv`;
+  errorCellNumberSample = `${ABSOLUTE_PATH}/BSA2019-Testing/samples/cart-cell-number-error.csv`;
+});
+
+beforeEach(() => {
+  parser = new CartParser();
+  validate = parser.validate.bind(parser);
+  readFile = parser.readFile.bind(this);
+  parse = parser.parse.bind(parser);
+  parseLine = parser.parseLine.bind(parser);
+});
+
+describe("CartParser - unit tests", () => {
+  beforeEach(() => {
+    //given
+    csvLine = `Mollis consequat,9.00,2`;
+    contentHeaderError = parser.readFile(errorHeaderSample);
+    contentRowCellsError = parser.readFile(errorRowCellsSample);
+    contentCellStringError = parser.readFile(errorCellStringSample);
+    contentCellNumberError = parser.readFile(errorCellNumberSample);
+  });
+  // Add your unit tests here.
+  it("should returned object with defined properties after parsing", () => {
+    //when
+    let parsedLine = parser.parseLine(csvLine);
+    //then
+    expect(parsedLine).toHaveProperty("id", "name", "price", "quantity");
+  });
+
+  it("should returned object with  property 'name' to be equal given value", () => {
+    //when
+    let parsedLine = parser.parseLine(csvLine);
+    //then
+    expect(parsedLine["name"]).toEqual(`Mollis consequat`);
+  });
+
+  it("should returned object with  property 'price' to be equal given value", () => {
+    //when
+    let parsedLine = parser.parseLine(csvLine);
+    //then
+    expect(parsedLine["price"]).toEqual(9);
+  });
+  it("should returned object with  property 'quantity' to be equal given value", () => {
+    //when
+    let parsedLine = parser.parseLine(csvLine);
+    //then
+    expect(parsedLine["quantity"]).toEqual(2);
+  });
+
+  it("should throw header error if file doesn't pass the validation", () => {
+    //when
+    let validation = parser.validate(contentHeaderError);
+    //then
+    expect(validation[0].message).toMatch(/Expected header to be named/);
+  });
+
+  it("should throw row cells error if file doesn't pass the validation", () => {
+    //when
+    let validation = parser.validate(contentRowCellsError);
+    //then
+    expect(validation[0].message).toMatch(/Expected row /);
+  });
+
+  it("should throw cell type error if the cell isn't of type string", () => {
+    //when
+    let validation = parser.validate(contentCellStringError);
+    //then
+    expect(validation[0].message).toMatch(
+      /Expected cell to be a nonempty string /
+    );
+  });
+
+  it("should throw cell type error if the cell isn't of type number", () => {
+    //when
+    let validation = parser.validate(contentCellNumberError);
+    //then
+    expect(validation[0].message).toMatch(
+      /Expected cell to be a positive number /
+    );
+  });
+});
+
+describe("CartParser - integration test", () => {
+  beforeEach(() => {});
+  // Add your integration test here.
+});
